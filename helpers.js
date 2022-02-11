8e61fb548f5ed03e863fbf1e471b08a7ba3ee28d
Itz-MeBeast1 <95710494+Itz-MeBeast1@users.noreply.github.com>
2022-02-12 02:12:56 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *RAGANORK*:  ```" + msg + "```"
+    return "✅ *EMILIA-TAN*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *RAGANORK*:  ```" + msg + "```"
+    return "🛑 *EMILIA-TAN*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *RAGANORK*:  ```" + msg + "```"
+    return "⏺️ *EMILIA-TAN*:  ```" + msg + "```"
 }
 
 
